1115677641ff63c6d97178db5b1996a29a095f61
Georgios Chantzialexiou <georgeha98@gmail.com>
2018-04-26 18:41:06 -0400

    working sign in using new html code

--- website/app.py
+++ website/app.py
@@ -5,7 +5,7 @@ import flask_login
 import flask
 from utils import User
 import utils
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 import json
 from flask import abort
@@ -20,7 +20,8 @@ login_manager.init_app(app)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    #path = 'UI/Home/login_page.html'
+    return  render_template('login_page.html')  
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,6 +29,8 @@ def login():
     # Here we use a class of some kind to represent and validate our
     # client-side form data. For example, WTForms is a library that will
     # handle this for us, and we use a custom LoginForm to validate.
+
+    """
     if flask.request.method == 'GET':
         return '''
                <form action='login' method='POST'>
@@ -36,39 +39,41 @@ def login():
                 <input type='submit' name='submit'/>
                </form>
                '''
-
-    email = flask.request.form['email']
-    users = utils.users
-    email = str(email)
-    print 'type is: ', type(email)
-    try:
-        print users[email]
-        if users[email]:
-            print 'it exists'
-    except:
-        print 'User does not exists'
-        print email
-        return 'Bad Login'
-
-
-    if flask.request.form['password'] == users[email]['password']:
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
-
-    return "Bad Login"
+   """
+    if flask.request.method =='POST':
+        print 'Test'
+        email = flask.request.form['email']
+        users = utils.users
+        email = str(email)
+        print 'type is: ', type(email)
+        try:
+            print users[email]
+            if users[email]:
+                print 'it exists'
+        except:
+            print 'User does not exists'
+            print email
+            return 'Bad Login'
 
 
-    next = flask.request.args.get('next')
+        if flask.request.form['password'] == users[email]['password']:
+            user = User()
+            user.id = email
+            flask_login.login_user(user)
+            return flask.redirect(flask.url_for('protected'))
+
+        return "Bad Login"
+
+
+        next = flask.request.args.get('next')
     # is_safe_url should check if the url is safe for redirects.
     # See http://flask.pocoo.org/snippets/62/ for an example.
-    if not is_safe_url(next):
-        return flask.abort(400)
+        if not is_safe_url(next):
+            return flask.abort(400)
 
-        return flask.redirect(next or flask.url_for('index'))
+            return flask.redirect(next or flask.url_for('index'))
 
-    return flask.render_template('login.html', form=form)
+    return render_template('login_page.html')   #login_page.html
 
 
 @app.route('/protected')
