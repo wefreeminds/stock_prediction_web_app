dd7be011403005963758588cd7d48a55c9821b24
Aniket Anilkumar <38740000+a-aniket19@users.noreply.github.com>
2018-04-25 21:16:51 -0400

    Add files via upload

---
+++ website/queries/min.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import requests
+import datetime as DT
+import time
+import csv
+import mysql.connector
+cnx = mysql.connector.connect(user='root',password='Anilraji95*',database='pp1_real_time')
+cursor = cnx.cursor()
+
+#stock_names =['AMZN', 'PETX', 'AABA', 'AAPL', 'NVDA','TXN', 'NDAQ', 'GOOG', 'FB', 'GS']
+#for stock_name in stock_names:
+	#print(stock_name)
+find_average=("select min(close)"
+	"from (select*"
+	"from pp1_historical.historical_aapl order by date DESC LIMIT 10)as T;")
+cursor.execute(find_average);
+result = cursor.fetchall()
+for i in result:
+    maximum= float(i[0])
+    print(maximum)
+cnx.commit()
+cursor.close()
+cnx.close()
