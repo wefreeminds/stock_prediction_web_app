11e4ae238025da4ab7427b3147d0c0181570131c
Georgios Chantzialexiou <georgeha98@gmail.com>
2018-04-26 23:10:06 -0400

    add plot predictions to utils

--- website/utils.py
+++ website/utils.py
@@ -20,3 +20,31 @@ users = {
 
 
         }
+
+
+def plot_predictions(data):
+    import matplotlib.pyplot as plt
+
+
+
+    predictions = data['predictions']
+    if  str(predictions[0].keys()[0])=='day' or str(predictions[0].keys()[0])=='day':
+        day_or_minutes = 'day-'
+    else:
+        day_or_minutes = 'minute-'
+    x_axis = [ day_or_minutes + str( i+1) for i in xrange(len(predictions))]
+    y_axis = []
+
+    for value in predictions:
+        y_axis.append(value['predicted_price'])
+
+    fig = plt.figure()
+
+    plt.title('Stock prediction')
+    plt.plot(x_axis,y_axis)
+    plt.ylabel('Stock Price in  $')
+    plt.grid(True)
+    fig.savefig('stock_predictions.png')
+
+   # plt.show()
+    return 'stock_predictions.png'
